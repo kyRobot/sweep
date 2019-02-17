3cd6be4b0c8589e18d5359e3f44847ff338be8d4
kyRobot <kylemilner@me.com>
2019-02-17 16:10:57 +0700

    new Game component

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board"
+
+
+class Game extends React.Component {
+
+    render() {
+        return (
+            <Board />
+        );
+    }
+}
+
+export default Game;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Board from './Board'
+import Game from './Game'
 
 ReactDOM.render(
   <div>
-    <Board />
+    <Game />
   </div>,
   document.getElementById("root")
 );
