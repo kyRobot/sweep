9ccf474642eddaa8997b09de3071848005b17f15
kyRobot <kylemilner@me.com>
2019-02-23 11:58:15 +0700

    lift game mechanics up out of Board into Game

--- src/Board.js
+++ src/Board.js
@@ -19,109 +19,14 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    const squares = props.minefield.slice().fill();
-    this.state = {
-      squares: squares,
-      dead: false
-    };
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.props.tiles[i]}
         key={i}
-        onClick={() => this.handleClick(i)}
+        onClick={() => this.props.onClick(i)}
       />
     );
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (squares[i] || this.state.dead) {
-      return;
-    }
-    let dead = false;
-    if (this.props.minefield[i]) {
-      squares[i] = "*";
-      dead = true;
-    } else {
-      this.walkout([i], this.props.columns, squares, []);
-    }
-
-    this.setState({
-      squares: squares,
-      dead: dead
-    });
-  }
-
-  mines(tiles) {
-    return tiles.map(t => this.props.minefield[t]).filter(Boolean).length;
-  }
-
-  get xyMovements() {
-    // 0,0 is top left so north & south are flipped
-    return [
-      [0, -1], // n
-      [1, 0], // e
-      [0, 1], // s
-      [-1, 0], // w
-      [-1, -1], // nw
-      [1, -1], // ne
-      [-1, 1], // sw
-      [1, 1] // se
-    ];
-  }
-
-  walkout(tiles, columns, squares, visited) {
-    if (tiles.length === 0) {
-      return;
-    }
-    let i = tiles.pop();
-    visited.push(i);
-    let neighbours = this.neighbours(i, columns);
-    let mines = this.mines(neighbours);
-    if (mines === 0) {
-      neighbours.forEach(neighbour => {
-        if (!visited.includes(neighbour) && !tiles.includes(neighbour)) {
-          tiles.push(neighbour);
-        }
-      });
-    }
-    squares[i] = mines;
-    this.walkout(tiles, columns, squares, visited);
-  }
-
-  neighbours(i, columns) {
-    // i = x + cols * y
-    // x = i % width
-    // y = i / width
-    return this.xyMovements
-      .map(move =>
-        this.get1DIndex(
-          i % columns,
-          Math.floor(i / columns),
-          columns,
-          move[0],
-          move[1]
-        )
-      )
-      .filter(Number.isInteger);
-  }
-
-  get1DIndex(originX, originY, columns, moveX, moveY) {
-    if (
-      !(
-        originX + moveX < 0 ||
-        originX + moveX >= columns ||
-        originY + moveY < 0 ||
-        originY + moveY >= columns
-      )
-    ) {
-      return originX + moveX + columns * (originY + moveY);
-    }
   }
 
   renderRow(row, totalRows) {
@@ -143,15 +48,9 @@ class Board extends React.Component {
   }
 
   render() {
-    let status;
-    if (this.state.dead) {
-      status = "Uh oh. Game over.";
-    } else {
-      status = "Avoid the " + this.props.mines + " mines!";
-    }
     return (
       <div>
-        <div className="board-row status">{status}</div>
+        <div className="board-row status">{this.props.status}</div>
         {this.renderBoard(this.props.columns)}
       </div>
     );
--- src/Game.js
+++ src/Game.js
@@ -2,19 +2,131 @@ import React from "react";
 import Board from "./Board";
 
 class Game extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const columns = 5;
     const mines = 4;
-    const minefield = Array(columns*columns).fill("*", 0, mines);
+    const minefield = Array(columns * columns).fill("*", 0, mines);
     shuffle(minefield);
+    const tiles = minefield.slice().fill();
+    this.state = {
+      columns: columns,
+      mines: mines,
+      minefield: minefield,
+      tiles: tiles,
+      dead: false
+    };
+  }
+
+  render() {
+    let status;
+    if (this.state.dead) {
+      status = "Uh oh. Game over.";
+    } else {
+      status = "Avoid the " + this.state.mines + " mines!";
+    }
     return (
       <div>
-        <Board columns={columns} mines={mines} minefield={minefield} />
+        <Board
+          columns={this.state.columns}
+          mines={this.state.mines}
+          minefield={this.state.minefield}
+          tiles={this.state.tiles}
+          onClick={this.handleClick}
+          status={status}
+        />
         <div className="board-row">
           <button className="square double-width">Reset</button>
         </div>
       </div>
     );
+  }
+
+  // fat arrow function to avoid needing to bind(this)
+  handleClick = i => {
+    const tiles = this.state.tiles.slice();
+    if (tiles[i] || this.state.dead) {
+      return;
+    }
+    let dead = false;
+    if (this.state.minefield[i]) {
+      tiles[i] = "*";
+      dead = true;
+    } else {
+      this.walkout([i], this.state.columns, tiles, []);
+    }
+
+    this.setState({
+      tiles: tiles,
+      dead: dead
+    });
+  };
+
+  mines(tiles) {
+    return tiles.map(t => this.state.minefield[t]).filter(Boolean).length;
+  }
+
+  get xyMovements() {
+    // 0,0 is top left so north & south are flipped
+    return [
+      [0, -1], // n
+      [1, 0], // e
+      [0, 1], // s
+      [-1, 0], // w
+      [-1, -1], // nw
+      [1, -1], // ne
+      [-1, 1], // sw
+      [1, 1] // se
+    ];
+  }
+
+  walkout(targets, columns, tiles, visited) {
+    if (targets.length === 0) {
+      return;
+    }
+    let i = targets.pop();
+    visited.push(i);
+    let neighbours = this.neighbours(i, columns);
+    let mines = this.mines(neighbours);
+    if (mines === 0) {
+      neighbours.forEach(neighbour => {
+        if (!visited.includes(neighbour) && !targets.includes(neighbour)) {
+          targets.push(neighbour);
+        }
+      });
+    }
+    tiles[i] = mines;
+    this.walkout(targets, columns, tiles, visited);
+  }
+
+  neighbours(i, columns) {
+    // i = x + cols * y
+    // x = i % width
+    // y = i / width
+    return this.xyMovements
+      .map(move =>
+        this.get1DIndex(
+          i % columns,
+          Math.floor(i / columns),
+          columns,
+          move[0],
+          move[1]
+        )
+      )
+      .filter(Number.isInteger);
+  }
+
+  get1DIndex(originX, originY, columns, moveX, moveY) {
+    if (
+      !(
+        originX + moveX < 0 ||
+        originX + moveX >= columns ||
+        originY + moveY < 0 ||
+        originY + moveY >= columns
+      )
+    ) {
+      return originX + moveX + columns * (originY + moveY);
+    }
   }
 }
 
